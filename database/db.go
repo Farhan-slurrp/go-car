e2c040510a8d4f162243e92bc5a80179038b7027
Farhan Ahmad Nurzi <58872254+Farhan-slurrp@users.noreply.github.com>
2024-05-30 07:40:05 +0800

    Update db.go

--- database/db.go
+++ database/db.go
@@ -8,7 +8,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := `postgres://postgres.vfofoxaunwukvggzbwoq:S4p3r$dmin~@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres`
+	dsn := ``
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
